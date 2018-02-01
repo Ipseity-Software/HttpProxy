aecaa8e340ec11452d99e7005d45c8ad9ca23204
Ethan Laur <phyrrus9@gmail.com>
2018-02-01 13:36:08 -0800

    fix debug stuff

--- plugins/static_resolve/static.c
+++ plugins/static_resolve/static.c
@@ -3,12 +3,8 @@
 #include <string.h>
 char *resolve(const char *host)
 {
-	printf("\t--In plugin static, host: %s\n", host);
+	printf("\t--In plugin static.resolve, host: %s\n", host);
 	if (!strcmp(host, "test2.fake"))
-	{
-		printf("\t-- -- passing back 2.2.2.2\n");
 		return "2.2.2.2";
-	}
-	printf("\t-- -- passing back NULL\n");
 	return NULL;
 }
